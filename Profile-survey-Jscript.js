9d88aee2114c23936d18f9fb67cb79c2c7ab14ed
Ghibrasoft <96905686+Ghibrasoft@users.noreply.github.com>
2022-10-04 13:00:24 +0400

    Add files via upload

---
+++ Profile-survey-Jscript.js
@@ -0,0 +1,8 @@
+function myFunc(str) {
+    alert ("Your form successfully submited!\nYou'll get your diet and workout instructions soon!");
+}
+
+function bgClrFunc(str) {
+   const test = document.getElementById(str);
+   test.style.backgroundColor = "lightgreen";
+}
